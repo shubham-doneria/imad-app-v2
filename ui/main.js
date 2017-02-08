3d0945bfbbcc17162f7c2d18a26a4be903008e13
shubham-doneria <shubham.sharma6080@gmail.com>
2017-02-08 20:42:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 // move the image
 var img = document.getElementById("hasura");
+function moveRight() {
+var marginLeft = 0;
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 10);
 };
