7c4f82b1218ef405706298deb472d006d5a35213
shubham-doneria <shubham.sharma6080@gmail.com>
2017-02-16 16:30:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,24 +77,21 @@ var htmlTemplate =
         return htmlTemplate;
     }
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 var counter = 0;
 app.get('/c/counter', function (req, res) {
     counter = counter + 1;
   res.send(counter.toString());
-});    
-
+});
 
 app.get('/:articleName', function (req, res) {
     //articleName == article-one,two,three
     //articles[articleName] == {} content object for article-one,two,three
    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
-});
-
-
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
