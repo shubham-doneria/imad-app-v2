53999427fea4273d2252acfd4a529e431d62b884
shubham-doneria <shubham.sharma6080@gmail.com>
2017-02-16 16:28:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var counter = 0;
-app.get('counter', function (req, res) {
+app.get('/c/counter', function (req, res) {
     counter = counter + 1;
   res.send(counter.toString());
 });
