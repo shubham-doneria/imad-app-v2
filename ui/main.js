ca07caedf024eded48643b00c7bfc9f1966c2114
shubham-doneria <shubham.sharma6080@gmail.com>
2017-02-16 20:04:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,5 @@
 console.log('Loaded!');
+
 // move the image
 var img = document.getElementById("profile");
 var marginLeft = 0;
@@ -9,4 +10,21 @@ function moveRight() {
 }
 img.onclick = function() {
     var interval = setInterval(moveRight, 30);
+};
+
+// counter code
+var counter = 0;
+var button = document.getElementById("counter");
+
+button.onclick = function() {
+    
+    //make a request to the counter endpoint
+    
+    //capture a response and store it in a variable
+    
+    //render the variable in the current span
+    
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
 };
