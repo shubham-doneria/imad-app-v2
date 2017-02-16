8c64d1723e5d71902280a309f9865f04e33e5c48
shubham-doneria <shubham.sharma6080@gmail.com>
2017-02-16 21:43:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,13 +18,27 @@ var button = document.getElementById("counter");
 
 button.onclick = function() {
     
-    //make a request to the counter endpoint
+    //create a request object
+    
+    var request = new XMLHttpRequest();
     
     //capture a response and store it in a variable
-    
-    //render the variable in the current span
-    
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+            //No action
+        }
+    };
+    // Make a request
+    request.open('GET', 'http://shubham-doneria.imad.hasura-app.io/', true);
+    request.send(null);
 };
+    
+    
+    
+    
