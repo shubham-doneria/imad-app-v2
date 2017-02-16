fd191cd8b1391edf95115523c35143e7c50ddca5
shubham-doneria <shubham.sharma6080@gmail.com>
2017-02-16 21:57:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,4 @@
-console.log('Loaded!');
 
-// move the image
-var img = document.getElementById("profile");
-var marginLeft = 0;
-
-function moveRight() {
-    marginLeft = marginLeft + 0.5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 30);
-};
 
 // counter code
 var button = document.getElementById("counter");
