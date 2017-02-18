8a3c3855d57562999c35d68b699b4299046ebe02
shubham-doneria <shubham.sharma6080@gmail.com>
2017-02-18 15:40:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -66,7 +66,7 @@ submit.onclick = function() {
     };
     // Make a request
     var name = nameInput.value;
-    request.open('GET', 'http://shubham-doneria.imad.hasura-app.io/submit-name', name, true);
+    request.open('GET', 'http://shubham-doneria.imad.hasura-app.io/submit-name?=name=' + name, true);
     request.send(null);
     //capture a list of names and render it as a list
    
